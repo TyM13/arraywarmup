13e2f54ff125f6245b39eb78ac74474f7ea2f4c9
Your Name <you@example.com>
2022-07-09 10:21:10 -0600

    WIP arraywarmup

---
+++ js/app.js
@@ -0,0 +1,28 @@
+let student_name = `tyler`;
+
+let student_gpa = 3.2;
+
+let honor_role = true;
+
+if (honor_role = true) {
+    console.log(`nice message`);
+} else if (honor_role = false) {
+    console.log(`encouraging message`);
+} else {
+    console.log(`error`);
+}
+
+
+if (student_gpa > 0  1.9) {
+    console.log(`f`);
+} else if (student_gpa = 2.0 - 2.4) {
+    console.log(`d`);
+} else if (student_gpa = 2.5 - 2.9){
+    console.log(`c`);
+}else if (student_gpa = 3.0 - 3.4){
+    console.log(`b`);
+}else if (student_gpa = 3.5 - 4.0){
+    console.log(`a`);
+}else {
+    console.log(`error`);
+}
